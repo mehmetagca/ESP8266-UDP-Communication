7bd93492a1b35c769eac87bf4829fe34592f37fa
Mehmet Ağca <mehmet.agca@yandex.com>
2019-05-12 16:22:07 +0300

    Added Wifi Signal Value To UDP Packet

--- EspUdpSlaveTest/EspUdpSlaveTest.ino
+++ EspUdpSlaveTest/EspUdpSlaveTest.ino
@@ -12,9 +12,9 @@ bool tickerOccured = false;
 char packetBuffer[255];
 unsigned int localPort = 9999;
 IPAddress ipServer(192, 168, 4, 1);
-IPAddress ipClient1(192, 168, 4, 10);
-IPAddress ipClient2(192, 168, 4, 11);
-IPAddress ipClient3(192, 168, 4, 12);
+IPAddress ipClient1(192, 168, 4, 10); //For Client1
+IPAddress ipClient2(192, 168, 4, 11); //For Client2
+IPAddress ipClient3(192, 168, 4, 12); //For Client3
 IPAddress Subnet(255, 255, 255, 0);
 
 void ticker_handler(){
@@ -27,7 +27,7 @@ void setup() {
   ticker.attach(1, ticker_handler);
   WiFi.begin(ssid, password);
   WiFi.mode(WIFI_STA); // STA important !!!
-  WiFi.config(ipClient2, ipServer, Subnet);
+  WiFi.config(ipClient1, ipServer, Subnet); //For Client1. Change 'ipClient1' For Other Clients!
   Udp.begin(localPort);
   delay(1000);
 }
@@ -38,11 +38,17 @@ void loop() {
     Udp.beginPacket(ipServer,9999);
     char sbuf[128];
     int serialKontrol = 1;
-    
+
+    long rssi = 0;
     char testData[255];
-    if(Serial.available() < 1)//Is Serial Available?
-      serialKontrol = 0;
-      //strcpy(testData,"Test"); //millis();
+    if(Serial.available() < 1)//Is Not Serial Available?
+      {
+        serialKontrol = 0;
+        //strcpy(testData,"Test"); //millis();
+
+        // get the received signal strength:
+        rssi = WiFi.RSSI();
+      }    
     else
     {
       int len = Serial.readBytes(testData, 255);
@@ -50,16 +56,17 @@ void loop() {
          testData[len] = 0;
     }
 
-    if(serialKontrol == 1)
-    {
-      //sprintf(sbuf, "%lu", testData);
-      sprintf(sbuf, "%s", testData);   
+    //if(serialKontrol == 1)
+    //{
+      sprintf(sbuf, "%ld", rssi); // Wifi Strength
+      //sprintf(sbuf, "%s", testData); Test For Serial
       Udp.write(sbuf);
       Udp.write("\r\n");
       Udp.endPacket();
-      Serial.print("S : ");Serial.println(sbuf);
+      Serial.print("S : ");
+      Serial.println(sbuf);
       delay(70);
-    }
+    //}
  
 //***************** Receive from server ****************************************************
   int packetSize = Udp.parsePacket();
